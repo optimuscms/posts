0dfd17e18885efedd5a573d958a232350bc63425
Jack <jack@optixsolutions.co.uk>
2018-06-29 14:35:01 +0100

    Tags

--- src/Post.php
+++ src/Post.php
@@ -39,7 +39,7 @@ class Post extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(PostTag::class, 'post_tag');
+        return $this->belongsToMany(PostTag::class, 'post_tag', 'post_id', 'tag_id');
     }
 
     public function comments()
